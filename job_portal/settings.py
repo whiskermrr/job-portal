2caaef5e82d920afe3c7be63ede0fb274a6342c4
Wiktor <witek119@gmail.com>
2017-12-28 23:02:09 +0100

    Added some stuff
    -basic models
    -settings update
    -urls update

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'jobs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "jobs/static")
+]
+
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "job_portal/static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "job_portal/media_cdn")
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,79 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class JobOffer(models.Model):
+    INDUSTRY_TYPES = (
+        ('Accounting', 'accounting'),
+        ('Factory' , 'factory'),
+        ('Banking', 'banking'),
+        ('Marketing', 'marketing'),
+        ('IT - Software Development', 'it - software development'),
+        ('IT - Administration', "it - administration"),
+        ('Transport', 'transport'),
+        ('Medical', 'medical'),
+        ('Education', 'education'),
+        ('Hospitality', 'hospitality'),
+        ('Real Estate', 'real estate'),
+        ('Production', 'production'),
+        ('Engineering', 'engineering'),
+        ('Other', 'other'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    industry = models.CharField(choices=INDUSTRY_TYPES)
+    avatar = models.ImageField(upload_to='media/avatars')
+    title = models.CharField(max_length=70)
+    aboutUs = models.CharField(max_length=2000)
+    requirements = models.CharField(max_length=500)
+    whatWeOffer = models.CharField(max_length=500)
+    salary = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    created_date = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=120)
+
+
+
+
+class ApplicationRequirements(models.Model):
+    job_offer = models.ForeignKey(JobOffer, on_delete=models.CASCADE, blank=True)
+    formPicture = models.BooleanField()
+    formAge = models.BooleanField()
+    formEducation = models.BooleanField()
+    formPlaceOfResidence = models.BooleanField()
+    formAboutYou = models.BooleanField()
+    formCurrentStatus = models.BooleanField()
+    formLanguages = models.BooleanField()
+    formExperience = models.BooleanField()
+    formHobby = models.BooleanField()
+
+
+
+class JobApplication(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    job_offer = models.ForeignKey(JobOffer, on_delete=models.CASCADE, blank=True)
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to='media/avatars')
+    age = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
+    EDUCATION_TYPES = (
+        ('Secondary Education', 'secondary education'),
+        ('Bachelor Degree', 'bachelor degree'),
+        ('Master Degree', 'master degree'),
+        ('Engineer', 'engineer'),
+        ('Other', 'other'),
+    )
+    education = models.CharField(choices=EDUCATION_TYPES)
+    placeOfResidence = models.CharField(max_length=120)
+    aboutYou = models.CharField(max_length=1000)
+
+    STATUS_TYPES = (
+        ('Student', 'student'),
+        ('Working - The same industry', 'working - the same industry'),
+        ('Working - Other industry', 'working - other industry'),
+        ('Unemployed', 'unemployed')
+    )
+    currentStatus = models.CharField(choices=STATUS_TYPES)
+    languages = models.CharField(max_length=100)
+    experience = models.CharField(max_length=1000)
+    hobby = models.CharField(max_length=300)
